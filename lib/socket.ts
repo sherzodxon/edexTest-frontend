0a36e2cbc4bda25c6e564d9490334caf8ae6dc05
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2026-01-20 23:17:17 +0500

    responsive is admin panel socket rest\et

--- lib/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io(process.env.NEXT_PUBLIC_BASE_API || "http://localhost:5000", {
-  transports: ["websocket"],
-  autoConnect: false, // login bo‘lgach ulanadi
-});
-
-export default socket;
